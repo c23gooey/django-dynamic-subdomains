26047b6144cbf0628069bb8af1479a1ad9a39772
Chris Lamb <chris@chris-lamb.co.uk>
2017-02-24 13:03:40 +0800

    Use Django's import_string over our own method.

--- dynamic_subdomains/apps.py
+++ dynamic_subdomains/apps.py
@@ -5,9 +5,9 @@ from django.apps import AppConfig
 from django.http import HttpRequest
 from django.conf.urls import url, include
 from django.test.client import RequestFactory
+from django.utils.module_loading import import_string
 
-from .utils import from_dotted_path, HttpRequest__get_host, \
-    RequestFactory__generic
+from .utils import HttpRequest__get_host, RequestFactory__generic
 from .app_settings import app_settings
 
 class DynamicSubdomainsConfig(AppConfig):
@@ -18,7 +18,7 @@ class DynamicSubdomainsConfig(AppConfig):
             # We add a literal period to the end of every pattern to avoid rather
             # unwieldy escaping in every definition.
             x['_regex'] = re.compile(r'%s(\.|$)' % x['regex'])
-            x['_callback'] = from_dotted_path(x['callback'])
+            x['_callback'] = import_string(x['callback'])
 
         if not app_settings.EMULATE:
             return
@@ -28,7 +28,7 @@ class DynamicSubdomainsConfig(AppConfig):
 
         # Inject our URLs
         for x in app_settings.SUBDOMAINS:
-            urlconf_module = from_dotted_path(x['urlconf'])
+            urlconf_module = import_string(x['urlconf'])
             urlconf_module.urlpatterns = list(urlconf_module.urlpatterns) + [
                 url(r'^_/subdomains/', include('dynamic_subdomains.urls',
                     namespace='dynamic-subdomains')),
--- dynamic_subdomains/utils.py
+++ dynamic_subdomains/utils.py
@@ -34,24 +34,6 @@ def get_subdomain(name):
     except KeyError:
         raise NoReverseMatch("No subdomain called %s exists" % name)
 
-def from_dotted_path(fullpath):
-    """
-    Returns the specified attribute of a module, specified by a string.
-
-    ``from_dotted_path('a.b.c.d')`` is roughly equivalent to::
-
-        from a.b.c import d
-
-    except that ``d`` is returned and not entered into the current namespace.
-    """
-
-    module, attr = fullpath.rsplit('.', 1)
-
-    return getattr(
-        __import__(module, {}, {}, (attr,)),
-        attr,
-    )
-
 def HttpRequest__get_host(self, *args, **kwargs):
     try:
         return self.COOKIES[app_settings.COOKIE_NAME]
